5f9a9bac2029cf903a65e07f1111df360d26c7ab
Karl McGuinness <kmcguinness@okta.com>
2014-02-07 16:52:27 -0800

    implemented class pattern for OktaClient

--- webclient.js
+++ webclient.js
@@ -1,36 +1,84 @@
 var RestClient = require('node-rest-client').Client;
-var oktaApi = new RestClient();
 
 
 
-function getActiveUsers (baseUrl, authToken) {
 
-   oktaApi.registerMethod("getActiveUsers", baseUrl + "/api/v1/users", "GET");
-   var authHeader = "SSWS " +  authToken;
-   var allUsersArgs = {
-      headers: {
-        "Accept":"application/json",
-        "Content-Type":"application/json",
-        "Authorization": authHeader
-      }
-    }
-    oktaApi.methods.getActiveUsers(allUsersArgs, 
+function userToAttributes(user) {
+  return {
+      objectClass: ['inetorgperson', 'organizationalperson', 'person', 'top'],
+      sn: user['profile']['lastName'],
+      givenName: user['profile']['firstName'],
+      mail: user['profile']['email'],
+      mobile: user['profile']['mobilePhone'],
+      uid: user['profile']['login']
+  };
+}
+
+
+
+// Constructor
+function OktaClient(baseUrl, apiToken) {
+  
+  var oktaApi = new RestClient();
+  var httpHeaders = {
+    "Accept":"application/json",
+    "Content-Type":"application/json",
+    "Authorization": apiToken
+  };
+
+  // Add Methods
+  oktaApi.registerMethod("getSingleActiveUser", baseUrl + "/api/v1/users/${uid}", "GET");
+  oktaApi.registerMethod("getActiveUsers", baseUrl + "/api/v1/users", "GET");
+
+  this.getUserByUid = function (uid, onSuccess, onFail) {
+    oktaApi.methods.getSingleActiveUser({
+        path: {"uid": uid },
+        headers: httpHeaders
+      },
+      function(data, response) {
+        if (response.statusCode == 200) {
+            console.log("Response Data:");
+            console.log(data);
+            console.log();
+            onSuccess(userToAttributes(JSON.parse(data)));
+        } else {
+          console.log("Wrong API Token!");
+          onFail();
+        }
+      }).on('error',function(err) {
+          console.log('something went wrong on the request', err.request.options);
+          onFail();
+      });
+  }
+
+  this.getUsers = function(onSuccess, onFail) {
+    oktaApi.methods.getActiveUsers( { headers: httpHeaders }, 
       function(data, response) {
         if (response.statusCode == 200) {
             console.log("Getting list of Active Users: \n");
             console.log(response);
             console.log(data);
-            transformToLdapRecords(JSON.parse(data));
-            //return next();
+
+            var users = JSON.parse(data);
+
+            var ldapUsers = [];
+            for (var i=0; i<users.length; i++) {
+              ldapUsers[i] = userToAttributes(users[i]);
+            }
+            return onSuccess(ldapUsers);
         } else {
           console.log("Wrong API Token!");
-          //return next(new ldap.InvalidCredentialsError());
+          onFail();
         }
       }).on('error',function(err) {
           console.log('something went wrong on the request', err.request.options);
-          //return next(new ldap.InvalidCredentialsError());
+          onFail();
     });
+
+  }
 }
+
+
 
 function getGroupsById (groupId, baseUrl, authToken) {
 
@@ -116,102 +164,5 @@ function getAllGroups (baseUrl, authToken) {
     });
 }
 
-function getUserByLogin (uid, baseUrl, authToken, callback) {
-  
 
-  oktaApi.registerMethod("getSingleActiveUser", baseUrl + "/api/v1/users/${uid}", "GET");
-  var singleUserArgs = {
-    path: {"uid": uid },
-    headers: {
-      "Accept":"application/json",
-      "Content-Type":"application/json",
-      "Authorization": "SSWS " +  authToken
-     }
-  }
-
-  console.log(singleUserArgs);
-
-  oktaApi.methods.getSingleActiveUser(singleUserArgs, 
-    function(data, response) {
-      if (response.statusCode == 200) {
-          console.log("Response Data:");
-          console.log(data);
-          console.log();
-          callback(userToAttributes(JSON.parse(data)));
-      } else {
-        console.log("Wrong API Token!");
-        //return next(new ldap.InvalidCredentialsError());
-      }
-    }).on('error',function(err) {
-        console.log('something went wrong on the request', err.request.options);
-        //return next(new ldap.InvalidCredentialsError());
-    });
-}
-
-function userToAttributes(user) {
-  return {
-      objectClass: ['inetorgperson', 'organizationalperson', 'person', 'top'],
-      sn: user['profile']['lastName'],
-      givenName: user['profile']['firstName'],
-      mail: user['profile']['email'],
-      mobile: user['profile']['mobilePhone'],
-      uid: user['profile']['login']
-  };
-}
-
-function transformToLdapRecords(data) {
-    var users = [];
-    console.log("num users: " + data.length);
-    for (var i=0; i<data.length; i++) {
-        console.log(data[i]);
-        users[i] = {
-            objectClass: ['inetorgperson', 'organizationalperson', 'person', 'top'],
-            sn: data[i]['profile']['lastName'],
-            givenName: data[i]['profile']['firstName'],
-            mail: data[i]['profile']['email'],
-            mobile: data[i]['profile']['mobilePhone'],
-            uid: data[i]['profile']['login']
-        };
-    }
-    console.log(users);
-}
-
-exports.getActiveUsers  = getActiveUsers;
-exports.getUserByLogin  = getUserByLogin;
-exports.getGroupsById   = getGroupsById;
-exports.getGroupsByName = getGroupsByName;
-exports.getAllGroups    = getAllGroups;
-/*var api = function (authHeader) {
-
-
-  var oktaApi = new RestClient();
-  oktaApi.registerMethod("getActiveUsers", orgBaseUrl + "/api/v1/users", "GET");
-
-   this.getActiveUsers = function() {
-   var allUsersArgs = {
-      headers: {
-        "Accept":"application/json",
-        "Content-Type":"application/json",
-        "Authorization": authHeader
-      }
-    }
-    oktaApi.methods.getActiveUsers(allUsersArgs, 
-      function(data, response) {
-        if (response.statusCode == 200) {
-            console.log("Getting list of Active Users: \n");
-            console.log(response);
-            console.log(data);
-            //return next();
-        } else {
-          console.log("Wrong API Token!");
-          //return next(new ldap.InvalidCredentialsError());
-        }
-      }).on('error',function(err) {
-          console.log('something went wrong on the request', err.request.options);
-          //return next(new ldap.InvalidCredentialsError());
-    });
-
- }
-
-
-}*/
+module.exports = OktaClient;
